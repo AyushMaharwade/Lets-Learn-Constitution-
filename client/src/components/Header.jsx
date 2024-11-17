b7f5fb739bcf00178cad9826dee95f44fdf17953
Rakesh Sinha <sinharakesh078@gmail.com>
2024-11-17 13:56:10 +0530

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,13 +6,13 @@ const Header = () => {
             <div className="md:h-[60vh] md:w-[100vw] h-[40vh] absolute z-[-1]  bg-gradient-to-t from-black to-transparent "></div>
             <div className="w-auto md:h-[40vh] flex flex-col justify-around items-center align- z-10">
                 <h1 className="md:text-5xl text-3xl font-medium text-white text-center font-sans underline">Sansthaein Aur Samvidhan Institutions & Constitution</h1>
-                <p className="md:w-[70vw] text-white font-medium md:inline hidden">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora eos suscipit repudiandae quis reiciendis distinctio laborum aliquam. Cumque possimus, ratione ipsam voluptas laudantium, maiores odio corporis, hic ullam vitae quae reiciendis animi. Corporis sunt aliquid quaerat facere dolores voluptatem amet facilis! Voluptas voluptatibus incidunt, reprehenderit illo distinctio accusantium tempora corrupti?</p>
+                <p className="md:w-[70vw] text-white font-medium md:inline hidden text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora eos suscipit repudiandae quis reiciendis distinctio laborum aliquam. Cumque possimus, ratione ipsam voluptas laudantium, maiores odio corporis, hic ullam vitae quae reiciendis animi. Corporis sunt aliquid quaerat facere dolores voluptatem amet facilis! Voluptas voluptatibus incidunt, reprehenderit illo distinctio accusantium tempora corrupti?</p>
             </div>
             <div className="h-[30vh] w-auto flex justify-center items-center">
 
             <div className="h-[30vh] w-[50vw] bg-gradient-to-r from-[#FB8500] to-[#FB8500] text-white p-4">
-                <div className="mb-3 mt-1 text-3xl font-semibold font-sans">About us</div>
-                <p className="text-left text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde optio vitae dicta. Eveniet accusantium, doloribus deleniti repellendus explicabo in officia fuga itaque corporis dolore quos soluta laudantium tempora temporibus maxime doloribus deleniti repellendus explicabo in officia fuga itaque corporis dolore quos soluta laudantium tempora temporibus maxime .</p>
+                <div className="mb-3 mt-1 text-3xl font-semibold font-sans text-center">About us</div>
+                <p className="text-left text-sm text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde optio vitae dicta. Eveniet accusantium, doloribus deleniti repellendus explicabo in officia fuga itaque corporis dolore quos soluta laudantium tempora temporibus maxime doloribus deleniti repellendus explicabo in officia fuga itaque corporis dolore quos soluta laudantium tempora temporibus maxime .</p>
             </div>
 
             </div>
