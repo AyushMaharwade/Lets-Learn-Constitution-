4af7ed131f2778ef07d3db4e5ace3ec0bc826189
divdub <129003637+divdub@users.noreply.github.com>
2024-12-02 23:24:06 +0530

    update responsive learn and games section

--- client/src/components/Learn.jsx
+++ client/src/components/Learn.jsx
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 import { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
@@ -27,16 +34,16 @@ const Learn = () => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center py-10">
       <div
         ref={containerRef}
-        className="w-[60%] h-[60vh] bg-blue-900 flex flex-col items-center justify-center rounded-md"
+        className="w-full sm:w-[80%] lg:w-[60%] h-auto sm:h-[60vh] bg-blue-900 flex flex-col items-center justify-center rounded-md p-5"
       >
-        <h1 className="text-white text-2xl">Learn</h1>
-        <p className="text-white mb-8">
+        <h1 className="text-white text-2xl sm:text-3xl">Learn</h1>
+        <p className="text-white text-center mb-8 px-2 sm:px-10">
           Unlock the knowledge of India&apos;s Constitution with engaging lessons and resources tailored for all.
         </p>
-        <div className="flex space-x-20">
+        <div className="flex flex-col sm:flex-row sm:space-x-24 space-y-5 sm:space-y-0">
           {/* Left Div */}
           <div
             className={`w-40 h-60 flex items-end justify-center p-4 rounded-md overflow-hidden shadow-md relative transform transition-transform duration-700 ${
@@ -98,99 +105,7 @@ const Learn = () => {
 
 export default Learn;
 
-// import { useEffect, useState, useRef } from "react";
-// import { Link } from "react-router-dom";
 
-// const Learn = () => {
-//   const [animate, setAnimate] = useState(false);
-//   const containerRef = useRef(null);
-
-//   useEffect(() => {
-//     const observer = new IntersectionObserver(
-//       ([entry]) => {
-//         if (entry.isIntersecting) {
-//           setAnimate(true);
-//         }
-//       },
-//       { threshold: 0.5 }
-//     );
-
-//     if (containerRef.current) {
-//       observer.observe(containerRef.current);
-//     }
-
-//     return () => {
-//       if (containerRef.current) {
-//         observer.unobserve(containerRef.current);
-//       }
-//     };
-//   }, []);
-
-//   return (
-//     <div className="flex items-center justify-center">
-//       <div
-//         ref={containerRef}
-//         className="w-[60%] h-[60vh] bg-blue-900 flex flex-col items-center justify-center rounded-md"
-//       >
-//         <h1 className="text-white text-2xl">Learn</h1>
-//         <p className="text-white mb-8">
-//         Unlock the knowledge of India's Constitution with engaging lessons and resources tailored for all
-//         </p>
-//         <div className="flex space-x-20">
-          
-//         <div className="w-40 h-60 flex items-end justify-center p-4 rounded-md overflow-hidden shadow-md relative">
-//               {/* Background Image */}
-//               <img
-//                 src="/Legislature.jpeg"
-//                 alt="Background"
-//                 className="absolute inset-0 w-full h-full object-cover hover:scale-105 transition-all duration-500 cursor-pointer"
-//               />
-//               {/* Button */}
-//               <Link to="/Legislative">
-//                 <button className="bg-orange-500 text-white py-2 px-4 rounded z-10 relative">
-//                   Read
-//                 </button>
-//               </Link>
-//             </div>
-
-//           {/* Middle Div */}
-//           <div
-//             className= "w-40 h-60 flex items-end justify-center p-4 rounded-md overflow-hidden shadow-md relative"
-//           >
-//             <img
-//               src="/Executive.jpg"
-//               alt="Background"
-//               className="absolute inset-0 w-full h-full object-cover hover:scale-105 transition-all duration-500 cursor-pointer"
-//             />
-//             <Link to="/Excecutive">
-//               <button className="bg-orange-500 text-white py-2 px-4 rounded z-10 relative">
-//                 Read
-//               </button>
-//             </Link>
-//           </div>
-
-//           {/* Right Div */}
-//           <div
-//             className="w-40 h-60 flex items-end justify-center p-4 rounded-md overflow-hidden shadow-md relative transform"
-//           >
-//             <img
-//               src="/Judiciary.jpg"
-//               alt="Background"
-//               className="absolute inset-0 w-full h-full object-cover hover:scale-105 transition-all duration-500 cursor-pointer"
-//             />
-//             <Link to="/Judiciary">
-//               <button className="bg-orange-500 text-white py-2 px-4 rounded z-10 relative">
-//                 Read
-//               </button>
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Learn;
 
 
 
